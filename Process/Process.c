a26ba0431db1fb7a3f2fa8c86e5ca71e8507edf7
Vi4eT <jon555jon@mail.ru>
2024-10-08 17:42:04 +0300

    Minor changes, refined comments

--- Process/Process.c
+++ Process/Process.c
@@ -1,20 +1,20 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include <windows.h>
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-  STARTUPINFO StartInfo; //атрибуты запуска
-  ZeroMemory(&StartInfo, sizeof(STARTUPINFO)); //инициализация
-  PROCESS_INFORMATION ProcessInfo; //данные о процессе (заполняется функцией CreateProcess)
-  ZeroMemory(&ProcessInfo, sizeof(PROCESS_INFORMATION)); //инициализация (только чтобы компилятор не ругался)
-  if(CreateProcess(TEXT("C:\\Windows\\notepad.exe"), NULL, NULL, NULL, FALSE, 0, NULL, NULL, &StartInfo, &ProcessInfo))
-  { //запуск блокнота со стандартными параметрами. макрос TEXT позволяет запускать проект и в Unicode, и в ASCII
-    Sleep(5000); //ожидание 5 сек
-    MessageBox(NULL, TEXT("Hello!"), TEXT("Message"), 0); //стандартное окно сообщения 
-    WaitForSingleObject(ProcessInfo.hProcess, INFINITE); //бесконечное ожидание завершения процесса
-    MessageBox(NULL, TEXT("Bye"), TEXT("Message"), 0);
+  STARTUPINFO StartInfo;                                        //атрибуты запуска
+  ZeroMemory(&StartInfo, sizeof(STARTUPINFO));                  //инициализация
+  StartInfo.cb = sizeof(StartInfo);                             //для корректной работы CreateProcess
+  PROCESS_INFORMATION ProcessInfo;                              //данные о процессе (заполняется CreateProcess)
+  ZeroMemory(&ProcessInfo, sizeof(PROCESS_INFORMATION));        //инициализация
+  if(CreateProcess(TEXT("C:\\Windows\\System32\\cmd.exe"), NULL, NULL, NULL, FALSE, 0, NULL, NULL, &StartInfo, &ProcessInfo))
+  { //запуск cmd со стандартными параметрами. макрос TEXT интерпретирует строку в текущей кодировке
+    Sleep(1000);                                                //ожидание 1 сек
+    MessageBox(NULL, TEXT("Hello"), TEXT("Message"), MB_OK);    //стандартное окно сообщения 
+    WaitForSingleObject(ProcessInfo.hProcess, INFINITE);        //бесконечное ожидание завершения процесса
+    MessageBox(NULL, TEXT("Bye"), TEXT("Message"), MB_OK);
+    CloseHandle(ProcessInfo.hProcess);                          //дескрипторы необходимо закрыть вручную
+    CloseHandle(ProcessInfo.hThread);                           //это не завершает ни процесс, ни поток!
+    return 0;
   }
-  CloseHandle(ProcessInfo.hProcess); //завершаясь, процесс не закрывает дескрипторы, необходимо вручную
-  CloseHandle(ProcessInfo.hThread); //это не завершает ни процесс, ни поток!
-  return 0;
+  return 1;
 }
